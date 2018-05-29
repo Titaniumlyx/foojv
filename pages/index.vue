2fd2e47dfbba18ca2b9324a400a9753d33e054ef
yao <6466617513@qq.com>
2018-05-30 02:39:04 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        我是主页
+    </div>
+</template>
+
+<script>
+    import axios from '~/plugins/axios'
+
+    export default {
+        async asyncData() {
+            let {data} = await axios.get('/api/users')
+            return {users: data}
+        },
+        head() {
+            return {
+                title: 'Users'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
