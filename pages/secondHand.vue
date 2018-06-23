8d437105e5a072dea146f3e08947ba3346f141d3
yao <6466617513@qq.com>
2018-06-23 16:39:49 +0800

    选项数据填充

--- pages/secondHand.vue
+++ pages/secondHand.vue
@@ -31,13 +31,38 @@
                         </el-checkbox-group>
                         <!--区域-->
                     </div>
-                    <div class="price clearfix mt20">
+                    <div class="price clearfix mt-20">
                         <!--价格选择-->
                         <span class="title">价格</span>
                         <!--价格选择结束-->
                         <el-checkbox-group v-model="formData.total_price">
                             <el-checkbox v-for="item in priceData" :key="item.id" :label="item.id">{{item.title}}</el-checkbox>
                         </el-checkbox-group>
+                    </div>
+                    <div class="custom-price clearfix mt-20">
+                        <span class="title fll">&nbsp;</span>
+                        <div class="custom-price-wrap fll">
+                            <input type="text" class="min-price" v-model="formData.minPrice">
+                            <span class="split">-</span>
+                            <input type="text" class="max-price" v-model="formData.maxPrice">
+                            <span class="units">万</span>
+                            <button class="custom-btn" @click="handleCustom">确定</button>
+                        </div>
+                    </div>
+                    <!--户型-->
+                    <div class="house-type mt-20">
+                        <span class="title">户型</span>
+                        <el-checkbox-group v-model="formData.bedroom">
+                            <el-checkbox v-for="item in bedroomData" :key="item.id" :label="item.id">{{item.title}}</el-checkbox>
+                        </el-checkbox-group>
+                    </div>
+                    <!--户型结束-->
+                    <div class="type mt-20">
+                        <span class="title">类型</span>
+                        <el-checkbox-group v-model="formData.type">
+                            <el-checkbox v-for="item in typeData"></el-checkbox>
+                        </el-checkbox-group>
+
                     </div>
 
                 </template>
@@ -62,6 +87,35 @@
             width: 96px;
             font-weight: 400;
         }
+        .custom-price-wrap {
+            input {
+                width: 56px;
+                height: 16px;
+                color: #999;
+                font-size: 14px;
+                border: 1px solid #ccc;
+                outline: none;
+                margin-left: 8px;
+            }
+            input:first-child {
+                margin-left: 0;
+            }
+            .split,.units {
+                margin-left: 8px;
+            }
+            .custom-btn {
+                background: #c30d23;
+                color: #fff;
+                width: 55px;
+                height: 25px;
+                font-size: 13px;
+                text-align: center;
+                line-height: 23px;
+                border: none;
+                margin-left: 8px;
+                outline: none;
+            }
+        }
     }
 </style>
 
@@ -83,10 +137,7 @@
             }
         },
         async asyncData() {
-            let regionData = await axios.get(api.paramToUrl(api.regionLists, {city: "呼和浩特"})).catch(err => {
-                console.log(err)
-            })
-            console.log(regionData)
+            let regionData = await axios.get(api.paramToUrl(api.regionLists, {city: "呼和浩特"}))
             return {
                 regionData: regionData.data.data
             }
@@ -95,7 +146,16 @@
             return {
                 formData: {
                     r_id: [],
-                    total_price: []
+                    total_price: [],
+                    bedroom: [],  //户型
+                    minPrice: "",
+                    maxPrice: "",
+                    type: [],
+                    decoration: [],
+                    built_area: [],
+                    minCustom_bulit_area: "",
+                    maxCustom_bulit_area: "",
+
                 },
                 priceData: [
                     {
@@ -118,7 +178,43 @@
                         title: "50万以上",
                         id: 4
                     }
-                ]
+                ],
+                bedroomData: [
+                    {title: "一室",id: 0},
+                    {title: "两室",id: 1},
+                    {title: "三室",id: 2},
+                    {title: "四室",id: 3},
+                    {title: "五室",id: 4}
+                ],
+                typeData: [
+                    {title: '平层',id: 0},
+                    {title: '复式',id: 1},
+                    {title: '跃层',id: 2}
+                ],
+                decorationData: [
+                    {title: '平层',id: 0},
+                    {title: '复式',id: 1},
+                    {title: '跃层',id: 2}
+                ],
+                directionData: [
+                    {title: '东',id: 0},
+                    {title: '西',id: 1},
+                    {title: '南',id: 2},
+                    {title: '北',id: 3},
+                    {title: '南北',id: 4},
+                    {title: '西南',id: 5},
+                    {title: '东北',id: 6},
+                    {title: '东北',id: 7},
+                    {title: '西北',id: 8},
+                    {title: '东西',id: 9}
+                ],
+                built_areaData: [
+                    {title: '不限',id: 0},
+                    {title: '50平以下',id: 1},
+                    {title: '50-70平',id: 2},
+                    {title: '70-90平',id: 3},
+                    {title: '90以上',id: 4}
+                ],
             }
         },
         components: {
@@ -127,6 +223,11 @@
             logoLink,
             filterBox,
             search
+        },
+        methods: {
+            handleCustom() {
+
+            }
         }
     }
 
